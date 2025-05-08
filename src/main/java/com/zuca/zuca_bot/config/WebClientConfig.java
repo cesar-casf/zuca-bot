abc7b7abe6b41bbcdb2ac1843ce9990a1b534702
Cesar <cesarmg1999@gmail.com>
2025-05-07 21:43:44 -0300

    adjust on webclient

--- src/main/java/com/zuca/zuca_bot/config/WebClientConfig.java
+++ src/main/java/com/zuca/zuca_bot/config/WebClientConfig.java
@@ -6,13 +6,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.unit.DataSize;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Setter
 @Configuration
 public class WebClientConfig {
 
-    @Setter
     @Value("${secret.football-api-token}")
     private String footballApiToken;
 
